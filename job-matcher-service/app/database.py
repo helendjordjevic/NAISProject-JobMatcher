7b9367bf81990ca5aeb1f2ae6bb96b413f83cb77
Helena <helendjordjevic@gmail.comgit config --global user.name Helenagit config --global user.email helendjordjevic@gmail.com>
2026-01-13 17:00:38 +0100

    initial commit

---
+++ job-matcher-service/app/database.py
@@ -0,0 +1,20 @@
+import os
+from pinecone import Pinecone
+from dotenv import load_dotenv
+
+load_dotenv()  
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
+INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+
+pc = Pinecone(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
+
+#try:
+    #indexes = pc.list_indexes().names()  
+    #if INDEX_NAME in indexes:
+    #    print(f"Povezano! Index '{INDEX_NAME}' postoji.")
+    #else:
+    #    print(f"Povezano, ali index '{INDEX_NAME}' ne postoji još.")
+#except Exception as e:
+    #print(f"Greška pri konekciji: {e}")
